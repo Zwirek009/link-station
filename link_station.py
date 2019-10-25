cee2852196a3469bb7db3774ec66fa22cfd33fc6
Zwirek009 <maciek.wiraszka@gmail.com>
2019-10-25 22:41:42 +0200

    implement get_distance and get_power functions

--- link_station.py
+++ link_station.py
@@ -1,15 +1,20 @@
 """This module calculates best link station (with most power) for a device at given point (x,y)"""
 
-# TODO
+import math
+
+def get_distance(point_a, point_b):
+    """Get distance between two (x,y) points"""
+    return math.sqrt((point_a[0]-point_b[0])**2 + (point_a[1]-point_b[1])**2)
+
 def get_power(point, link_station):
     """Get power from given link_station (x, y, reach) for given point (x, y)"""
-    raise NotImplementedError
+    distance = get_distance(point, (link_station[0], link_station[1]))
+    return (link_station[2]-distance)**2 if distance < link_station[2] else 0
 
 # TODO
 def get_best_link_station(point, link_stations):
     """Get best link station (with most power) for given point (x,y)"""
     return "TODO get_best_link_station for point" + str(point)
-
 
 def main():
     """Run program for exercise data"""
